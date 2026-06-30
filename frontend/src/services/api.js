31ac39659e2fe71edac9f0bd4348b101d4dfe707
ramaiahgaritharun-svg <ramaiahgaritharun2200@gmail.com>
2026-06-30 08:46:32 +0530

    fix api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cartifymt.up.railway.app/",
+  baseURL: "https://cartifymt.up.railway.app/api/",
 });
 
 // Attach token automatically to every request
